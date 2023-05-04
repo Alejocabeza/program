fabef48b32c9fff1163b5517947a23e75cf45193
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-05-03 23:42:44 -0400

    guia 1

---
+++ guia_1/2.cpp
@@ -0,0 +1,12 @@
+//Calcular el nuevo salario de un obrero si obtuvo un incremento del 25% sobre su salario anterior
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float salario;
+    cout << "Cual es tu salario: "; cin >> salario;
+    cout << "tu incremento es de: " << (salario*0.25) << "bs" << " tu salario actualizado es de: " << (salario*0.25) + salario << "bs";
+    return 0;
+}
