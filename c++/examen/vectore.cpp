6a3c42317ab583958e6709a03e0a9ad4e6c96bfd
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-06-07 11:22:46 -0400

    new exam

---
+++ c++/examen/vectore.cpp
@@ -0,0 +1,6 @@
+// la pizzeria el punto. tiene a su disposicion cuatro combos para su clientela por el mes de aniversario. cada combo tiene la siguiente informacion, codigo, cantidad vendida por fin de semana(viernes, sabado y domingo) y precio del combo. la empresa desea un programa en c++ que permita calcular lo siguiente.
+// 1.- cantidad por cada tipo de combo vendido el fin de semana.
+// 2.- Cantidad de dinero obtenido por cada tipo de combo el fin de semana
+// 3.- cantidad de dinero total obtenido durante el fin de semana.
+// 4.- Cual es el combo mas vendido durante el fin de semana.
+#include <iostream>
